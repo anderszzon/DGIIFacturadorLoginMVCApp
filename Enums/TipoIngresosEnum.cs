d088661711c9e72c2312dc3084bfb8f92a3bc122
Anderson Gordillo <49369634+anderszzon@users.noreply.github.com>
2026-05-10 10:47:20 -0500

    Insercion del enum tipo de ingresos

---
+++ Enums/TipoIngresosEnum.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DGIIFacturadorLoginMVCApp.Enums
+{
+    public enum TipoIngresosEnum
+    {
+        [Display(Name = "Seleccione")]
+        Enum0 = 0,
+
+        [Display(Name = "Ingresos por operaciones (No Financieros)")]
+        Enum1 = 1,
+
+        [Display(Name = "Ingresos Financieros")]
+        Enum2 = 2,
+
+        [Display(Name = "Ingresos Extraordinarios")]
+        Enum3 = 3,
+
+        [Display(Name = "Ingresos por Arrendamientos")]
+        Enum4 = 4,
+
+        [Display(Name = "Ingresos por Venta de Activo Depreciable")]
+        Enum5 = 5,
+
+        [Display(Name = "Otros Ingresos")]
+        Enum6 = 6
+    }
+}
---
+++ Helpers/EnumHelper.cs
@@ -0,0 +1,29 @@
+﻿using DGIIFacturadorLoginMVCApp.Enums;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace DGIIFacturadorLoginMVCApp.Helpers
+{
+    public static class EnumHelper
+    {
+        public static List<SelectListItem> ObtenerTipoIngresos()
+        {
+            return Enum.GetValues(typeof(TipoIngresosEnum))
+                .Cast<TipoIngresosEnum>()
+                .Select(e => new SelectListItem
+                {
+                    Value = (int)e == 0
+                        ? ""
+                        : ((int)e).ToString("D2"),
+
+                    Text = e.GetType()
+                            .GetMember(e.ToString())
+                            .First()
+                            .GetCustomAttribute<DisplayAttribute>()?
+                            .GetName() ?? e.ToString()
+                })
+                .ToList();
+        }
+    }
+}
